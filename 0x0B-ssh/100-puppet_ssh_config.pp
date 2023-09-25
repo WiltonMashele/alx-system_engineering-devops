780f7c9951d21f4c2fe3ac61984fc6a92c6e9e2b
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-24 22:53:34 -0700

    fixing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # changes to our configuration file.
 
-class ssh_config {
+class ssh_client_config {
 
-  file_line { 'Set Identity File':
+  file_line { 'ssh_identity_file':
     path => '/etc/ssh/ssh_config',
     line => '    IdentityFile ~/.ssh/school',
   }
 
-  file_line { 'Disable Password Authentication':
+  file_line { 'ssh_password_auth':
     path => '/etc/ssh/ssh_config',
     line => '    PasswordAuthentication no',
   }
