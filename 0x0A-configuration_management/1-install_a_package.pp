3b967cf0d4859ac0812357d3ea1b136f907ecc3f
WiltonMashele <Wiltonmashele@gmail.com>
2023-09-26 16:15:20 -0700

    fixing

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,7 @@
 #!/usr/bin/pup
 
 # This Puppet manifest installs a specific version of the Flask package.
-package { 'Flask Package Installation':
+package { 'flask':
   name     => 'flask',
   ensure   => '2.1.0',
   provider => 'pip3',
