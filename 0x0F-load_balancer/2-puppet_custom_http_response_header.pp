8bbcf025673b3e647fa912e6373a2071b37dfcae
WiltonMashele <Wiltonmashele@gmail.com>
2023-10-02 12:45:47 -0700

    fixing

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,12 +1,10 @@
 # automating the task of creating a custom HTTP header response
 include 'apt'
 package { 'nginx': ensure => installed, require => Class['apt'], }
-
 file_line { 'nginx_custom_header':
   path   => '/etc/nginx/sites-available/default',
   line   => '    add_header X-Served-By $hostname;',
   after  => '    listen 80 default_server;',
   notify => Service['nginx'],
 }
-
 service { 'nginx': ensure => running, enable => true, require => Package['nginx'], }
