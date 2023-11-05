502908ee7ea90462d167016e3a4768d0491c4dc8
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-04 22:02:15 -0700

    fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,39 +1,44 @@
 #!/usr/bin/python3
 """Returns information about the employee's TODO list progress."""
-
+import json
 import sys
-import requests
-import urllib.request
+import urllib.request as fetcher
+
+def fetch_user_data(user_id):
+    endpoint = 'https://jsonplaceholder.typicode.com'
+    name_url = f'/users/{user_id}'
+    todos_url = f'{name_url}/todos'
+
+    try:
+        with fetcher.urlopen(endpoint + name_url) as res:
+            user_data = res.read()
+        with fetcher.urlopen(endpoint + todos_url) as res:
+            todos_data = res.read()
+    except Exception as e:
+        print(f"Error: {e}")
+        sys.exit(1)
+
+    user = json.loads(user_data)
+    todos = json.loads(todos_data)
+
+    return user, todos
 
 def main():
     if len(sys.argv) != 2:
         print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
-    employee_id = sys.argv[1]
-    user_url = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    todo_url = f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-
-    try:
-        with urllib.request.urlopen(user_url) as response:
-            user_data = response.read().decode('utf-8')
-        with urllib.request.urlopen(todo_url) as response:
-            todo_data = response.read().decode('utf-8')
-    except urllib.error.URLError as e:
-        print(f"Error: {e.reason}")
-        sys.exit(1)
-
-    user = json.loads(user_data)
-    todos = json.loads(todo_data)
+    user_id = sys.argv[1]
+    user, todos = fetch_user_data(user_id)
 
     completed_tasks = [task for task in todos if task["completed"]]
     total_tasks = len(todos)
 
     print("Employee {} is done with tasks ({}/{}):".format(
-        user.get("name"), len(completed_tasks), total_tasks))
+        user["name"], len(completed_tasks), total_tasks))
 
     for task in completed_tasks:
-        print(f"\t{task['title']}")
+        print("\t{}".format(task["title"]))
 
 if __name__ == "__main__":
     main()
