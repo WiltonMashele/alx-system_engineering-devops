260f0afd4994b219341c70d9371bfd246d1d6e39
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-04 22:21:11 -0700

    fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     todos = requests.get(f"{url}todos", params={"userId": user_id}).json()
 
     completed = [t["title"] for t in todos if t["completed"]]
-    print(f"Employee {user['name']} is done with tasks({len(completed)}/{len(todos)}):")
+    task_count = len(todos)
+    print(f"Employee {user['name']} is done with tasks({len(completed)}/{task_count}):")
     [print(f"\t {c}") for c in completed]
