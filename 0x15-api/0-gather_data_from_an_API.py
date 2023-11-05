95fa3299d2abde4a49e87f942d9dc1933c85dbb4
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-04 22:18:25 -0700

    fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,14 @@
 #!/usr/bin/python3
 """Returns information about the employee's TODO list progress."""
-
-import json
+import requests
 import sys
-import urllib.request as fetcher
-
-def fetch_user_data(user_id):
-    endpoint = 'https://jsonplaceholder.typicode.com'
-    name_url = f'/users/{user_id}'
-    todos_url = f'/todos?userId={user_id}'
-
-    try:
-        with fetcher.urlopen(endpoint + name_url) as res:
-            user_data = res.read()
-        with fetcher.urlopen(endpoint + todos_url) as res:
-            todos_data = res.read()
-    except Exception as e:
-        print(f"Error: {e}")
-        sys.exit(1)
-
-    user = json.loads(user_data)
-    todos = json.loads(todos_data)
-
-    return user, todos
-
-def main():
-    if len(sys.argv) != 2:
-        print("Usage: {} <employee_id>".format(sys.argv[0]))
-        sys.exit(1)
-
-    user_id = int(sys.argv[1])  # Convert to an integer
-
-    user, todos = fetch_user_data(user_id)
-
-    completed_tasks = [task for task in todos if task["completed"]]
-    total_tasks = len(todos)
-
-    print("Employee {} is done with tasks ({}/{}):".format(
-        user["name"], len(completed_tasks), total_tasks))
-
-    for task in completed_tasks:
-        print("\t{}".format(task["title"]))
 
 if __name__ == "__main__":
-    main()
+    url = "https://jsonplaceholder.typicode.com/"
+    user_id = sys.argv[1]
+    user = requests.get(f"{url}users/{user_id}").json()
+    todos = requests.get(f"{url}todos", params={"userId": user_id}).json()
+
+    completed = [t["title"] for t in todos if t["completed"]]
+    print(f"Employee {user['name']} is done with tasks({len(completed)}/{len(todos)}):")
+    [print(f"\t {c}") for c in completed]
