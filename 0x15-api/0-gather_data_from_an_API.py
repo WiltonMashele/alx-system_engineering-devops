9a0511249e7eea43e69e5313a08b077b0bde6936
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-06 10:57:06 -0800

    fixing

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,12 +7,15 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     user_id = sys.argv[1]
     user = requests.get(f"{url}users/{user_id}").json()
-    todos = requests.get(f"{url}todos", params={"userId": user_id}).json()
+    todos = requests.get(
+        f"{url}todos", params={"userId": user_id}
+    ).json()
 
     completed = [t["title"] for t in todos if t["completed"]]
     task_count = len(todos)
+    employee_name = user['name']
     progress_message = (
-    f"Employee {employee_name} is done with tasks"
-    f"({len(completed)}/{task_count}):"
+        f"Employee {employee_name} is done with tasks({len(completed)}/{task_count}):"
     )
+    print(progress_message)
     [print(f"\t {c}") for c in completed]
