b5c92a19df6d598bb6555a15985b9545f5badb4f
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-06 11:37:55 -0800

    fixing

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,23 +1,37 @@
 #!/usr/bin/python3
-""" script to export data in the JSON format."""
+"""
+Python script to export data in the JSON format.
+Records all tasks from all employees
+"""
+import json
 import requests
 import sys
-import json
 
-if __name__ == "__main":
-    url = "https://jsonplaceholder.typicode.com/"
-    user_ids = range(1, 11)
-    all_data = {}
 
-    for user_id in user_ids:
-        user = requests.get(url + "users/{}".format(user_id)).json()
-        todos = requests.get(url + "todos", params={"userId": user_id}).json()
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    users_endpoint = f'{url}users'
+    users_response = requests.get(users_endpoint)
+    users_data = users_response.json()
 
-        user_tasks = [{"username": user.get("username"), "task": task.get("title"), "completed": task.get("completed")} for task in todos]
+    user_tasks = {}
+    for user_data in users_data:
+        name = user_data.get('username')
+        user_id = user_data.get('id')
+        todos_endpoint = f'{url}todos?userId={user_id}'
+        todos_response = requests.get(todos_endpoint)
+        tasks = todos_response.json()
+        task_list = []
+        for task in tasks:
+            task_dict = {
+                "username": name,
+                "task": task.get('title'),
+                "completed": task.get('completed')
+            }
+            task_list.append(task_dict)
 
-        all_data[user_id] = user_tasks
+        user_tasks[str(user_id)] = task_list
 
-    with open("todo_all_employees.json", "w") as json_file:
-        json.dump(all_data, json_file, indent=2)
-
-    print("Data exported to todo_all_employees.json")
+    filename = 'todo_all_employees.json'
+    with open(filename, mode='w') as file:
+        json.dump(user_tasks, file)
