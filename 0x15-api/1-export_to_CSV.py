6fa12d7d3416b18fb054daa2d5a111276fccd111
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-06 11:14:14 -0800

    fixing

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -8,32 +8,40 @@ import requests
 import sys
 
 
-def main():
-    url = "https://jsonplaceholder.typicode.com/"
-    user_id = sys.argv[1]
-    user = requests.get(f"{url}users/{user_id}").json()
-    todos = requests.get(f"{url}todos", params={"userId": user_id}).json()
-
-    completed = [t for t in todos if t["completed"]]
-    employee_name = user['name']
-
-    csv_filename = f"{user_id}.csv"
-    with open(csv_filename, mode='w', newline='') as csv_file:
-        fieldnames = [
-                "USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-        writer.writeheader()
-
-        for task in completed:
-            writer.writerow({
-                "USER_ID": user_id,
-                "USERNAME": employee_name,
-                "TASK_COMPLETED_STATUS": "completed",
-                "TASK_TITLE": task["title"]
-            })
+def fetch_user_data(userid, url):
+    user_url = f'{url}users/{userid}'
+    response = requests.get(user_url)
+    user_info = response.json()
+    username = user_info.get('username')
+    return username
 
 
-    print(f"Number of tasks in CSV: OK")
+def fetch_user_tasks(userid, username, url):
+    todos_url = f'{url}todos?userId={userid}'
+    response = requests.get(todos_url)
+    tasks = response.json()
+    task_list = []
+    for task in tasks:
+        task_list.append([userid, username, task.get('completed'), task.get('title')])
+    return task_list
+
+
+def save_tasks_to_csv(filename, task_list):
+    with open(filename, mode='w', newline='') as employee_file:
+        employee_writer = csv.writer(employee_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+        for task in task_list:
+            employee_writer.writerow(task)
+
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <userid>")
+        sys.exit(1)
+
+    url = 'https://jsonplaceholder.typicode.com/'
+    userid = sys.argv[1]
+
+    username = fetch_user_data(userid, url)
+    task_list = fetch_user_tasks(userid, username, url)
+    filename = f'{userid}.csv'
+    save_tasks_to_csv(filename, task_list)
