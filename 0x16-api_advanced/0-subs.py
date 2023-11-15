5c6bb53358438500430cbcb8c1cd72bd9d9ffadb
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-15 13:31:47 -0800

    fixing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
-"""Module to interact with Reddit API and retrieve subreddit information."""
+"""
+Module to interact with Reddit API and retrieve subreddit information.
+"""
 import requests
 
 
 def get_subreddit_subscribers(subreddit):
-    """Return the total number of subscribers for a specified subreddit."""
+    """
+    Return the total number of subscribers for a specified subreddit.
+    """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
     try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()  # Raise an HTTPError for bad responses
+        response = requests.get(url, headers=headers, 
+                allow_redirects=False)
+        response.raise_for_status()
         data = response.json()["data"]
         subscribers = data["subscribers"]
         return subscribers
