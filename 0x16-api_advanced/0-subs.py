24b3ad4d2130b50236fd6f524c26353189c91b06
WiltonMashele <Wiltonmashele@gmail.com>
2023-11-15 13:46:45 -0800

    fixing

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,17 +9,19 @@ def get_subreddit_subscribers(subreddit):
     """
     Return the total number of subscribers for a specified subreddit.
     """
+    headers = {"User-Agent": "0x16.API_advanced-WiltonMashele"}
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()["data"]
-        subscribers = data["subscribers"]
-        return subscribers
+
+        if response.status_code == 200:
+            subscribers = response.json().get("data", {}).get("subscribers", 0)
+            return subscribers
+        else:
+            print(f"Request failed with status code {response.status_code}")
+            return 0
+
     except requests.exceptions.RequestException as e:
         print(f"Error: {e}")
         return 0
